79bace961c2d1fb617fe572c54c21cab5a6df03a
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-10-06 18:40:49 +0200

    Add files via upload

---
+++ precache-manifest.830ffe88c2081975b93b6a0c26511c99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nh_test_web/robots.txt"
+  },
+  {
+    "revision": "7a76fe0eaaa7e1eac6f7",
+    "url": "/nh_test_web/js/chunk-vendors.251c64bf.js"
+  },
+  {
+    "revision": "7f0a72f5bad809c5ed69",
+    "url": "/nh_test_web/js/app.69997bfd.js"
+  },
+  {
+    "revision": "e4e5fae160d8506e1c73",
+    "url": "/nh_test_web/js/about.1d18f291.js"
+  },
+  {
+    "revision": "809414198f156f4f3dd448b1221986a1",
+    "url": "/nh_test_web/index.html"
+  },
+  {
+    "revision": "7f0a72f5bad809c5ed69",
+    "url": "/nh_test_web/css/app.d8ad63a8.css"
+  },
+  {
+    "revision": "e4e5fae160d8506e1c73",
+    "url": "/nh_test_web/css/about.60384e5d.css"
+  }
+];
